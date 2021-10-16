29f1ca7ffe36c102751f4031b02c88b136c3b2ab
luna <lunabright817@gmail.com>
2021-10-16 14:55:40 +0900

    init project

---
+++ src/components/Root.vue
@@ -0,0 +1,34 @@
+/* eslint-disable quotes */
+<template>
+  <span>{{ message }}</span>
+</template>
+
+<script>
+export default {
+  name: "Root",
+  data() {
+    return {
+      message: "this is root"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
