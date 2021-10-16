37af4ce67ab92f5da0d641f86fecef25324050be
luna <lunabright817@gmail.com>
2021-10-16 15:17:23 +0900

    로그인 시 값 받아오기

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="login">
     <h1>{{ message }}</h1>
-    <label>
-      ID
-      <input type="text" />
-    </label>
-    <label>
-      Password
-      <input type="password" />
-    </label>
-    <input type="submit" value="login" />
+    <form v-on:submit="onSubmit">
+      <label>
+        ID
+        <input type="text" v-model="id" />
+      </label>
+      <label>
+        Password
+        <input type="password" v-model="password" />
+      </label>
+      <input type="submit" value="login" />
+    </form>
   </div>
 </template>
 
@@ -18,8 +20,17 @@ export default {
   name: "Login",
   data() {
     return {
-      message: "Login Page"
+      message: "Login Page",
+      id: "",
+      password: ""
     };
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+      console.log(this.id);
+      console.log(this.password);
+    }
   }
 };
 </script>
