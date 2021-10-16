66fcd0e1c0e54bf4c1834f2a6dfd6d29bf12adf7
luna <lunabright817@gmail.com>
2021-10-16 15:02:33 +0900

    add login page

---
+++ src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      message: "Login Page"
+    };
+  }
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Root from "@/components/Root";
+import Login from "@/components/Login";
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: "/",
       name: "root",
       component: Root
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
     }
   ]
 });
